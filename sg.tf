52b8e47c350be8d4a615e8cda5b19cbc39cc9430
OgaSege <oluwasegunadedigba@gmail.com>
2023-02-19 15:37:19 +0100

    updated

--- sg.tf
+++ sg.tf
@@ -1,68 +1,65 @@
 resource "aws_security_group" "allow_tls" {
-  security_group_name       = "allow_tls"
-  description               = "Allow inbound TLS traffic"
-  vpc_id                    =  aws_vpc.main.id
-  
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
 
-  type              = "ingress"
-  description       = "TLS from VPC"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  
-  
-  type              = "ingress"
-  description       = "TLS from VPC"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
- tags = {
+  tags = {
     Name = "allow_tls"
   }
-  
 }
 
-
 resource "aws_security_group" "allow_tls_db" {
-  security_group_name       = "allow_tls_db"
-  description               = "Allow inbound TLS traffic"
-  vpc_id                    =  aws_vpc.main.id
-  
+  name        = "allow_tls_db"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
 
-  type              = "ingress"
-  description       = "TLS from VPC"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  
-  
-  type              = "ingress"
-  description       = "TLS from VPC"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
- tags = {
+  tags = {
     Name = "allow_tls_db"
   }
-  
 }
