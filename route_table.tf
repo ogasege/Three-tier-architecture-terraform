48366642a1a304694f4048321f0d7044e07cfb92
OgaSege <oluwasegunadedigba@gmail.com>
2023-02-19 15:44:40 +0100

    updated

--- route_table.tf
+++ route_table.tf
@@ -13,7 +13,7 @@ resource "aws_route_table" "rtb" {
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public[count.index]id
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.rtb.id
   count = 2
 }
